35d1eaa6dc23d12d1f6a33bd2839ac99336be6dc
alber748 <104584787+alber748@users.noreply.github.com>
2023-10-13 11:48:15 -0600

    firts commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h1>Hello Vite + Typescript</h1>
+`
+
